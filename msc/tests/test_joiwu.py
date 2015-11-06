e7ade757dec4d32ee8345482ad367c60460b5755
joiwu <s1514982@sms.ed.ac.uk>
2015-11-06 11:41:04 +0000

    Test "-" and "n"

--- msc/tests/test_joiwu.py
+++ msc/tests/test_joiwu.py
@@ -4,6 +4,8 @@ from msc.rot13 import rot13_char
 
 def test_rot13_char():
     assert_equal("n", rot13_char("a"))
+    assert_equal("-", rot13_char("-"))
+    assert_equal("a", rot13_char("n"))
 
 #def test_rot13():
 #    assert_equal("n", rot13("a"))
